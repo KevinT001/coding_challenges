ca2e2b130bc14ed01241f10924dc3e129b431f49
Kevin Ta <kvnta@yahoo.com>
2024-10-09 20:30:11 -0600

    add test case example at bottom

--- 8_kyu/string_bonusTime.js
+++ 8_kyu/string_bonusTime.js
@@ -18,6 +18,17 @@ function bonusTime(salary, bonus) {
     return ("£" +(salary * 10))
   } 
   else if(bonus == false){
-    return ("£" + salary)Do
+    return ("£" + salary)
   }
 }
+
+// describe("Basic tests",() =>{
+//   it("Testing for fixed tests", () => {
+//     assert.strictEqual(bonusTime(10000, true), '£100000');
+//     assert.strictEqual(bonusTime(25000, true), '£250000');
+//     assert.strictEqual(bonusTime(10000, false), '£10000');
+//     assert.strictEqual(bonusTime(60000, false), '£60000');
+//     assert.strictEqual(bonusTime(2, true), '£20');
+//     assert.strictEqual(bonusTime(78, false), '£78');
+//     assert.strictEqual(bonusTime(67890, true), '£678900');
+//   })

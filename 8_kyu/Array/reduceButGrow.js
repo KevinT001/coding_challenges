b2f08c677085b09d572679242350c1c2529721d0
Kevin Ta <kvnta@yahoo.com>
2024-10-17 15:56:09 -0600

    add solutions

--- 8_kyu/Array/reduceButGrow.js
+++ 8_kyu/Array/reduceButGrow.js
@@ -1,3 +1,27 @@
 /*Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
 
 [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24*/
+
+function grow(x){
+const multiply = x.reduce((accum, crntVal) => Number(accum) * crntVal,1)
+  return multiply
+  
+}
+
+console.log(grow([1,2,3,4]),24)
+console.log(grow(["1",2, "3",4]))
+
+//ABOVE FOR .REDUCE((accumulator, current value))
+
+
+/* const numbers = [1, 2, 3, 4];
+let product = 1;
+
+numbers.forEach(number => {
+  product *= number;
+});
+
+console.log(product); // Output: 24
+*/
+
+// above for forEach

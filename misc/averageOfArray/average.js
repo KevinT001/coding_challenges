d93b4d00f4d274adc16acadf80eabef8de7c31c2
Kevin Ta <kvnta@yahoo.com>
2024-10-16 19:51:50 -0600

    create folder for sum array and create average array and completed

---
+++ misc/averageOfArray/average.js
@@ -0,0 +1,17 @@
+/*It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+Return the average of the given array rounded down to its nearest integer.
+
+The array will never be empty.
+*/
+
+
+function getAverage(marks){
+  //TODO : calculate the downward rounded average of the marks array
+  
+const sum = marks.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+const avg = sum/marks.length
+
+return Math.floor(avg);
+}
+console.log(getAverage([3.2,5,1,2.5]),2)

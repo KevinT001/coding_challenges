795124b58457e3ce644d888cff1aa40c6bbd996b
Kevin Ta <kvnta@yahoo.com>
2024-10-18 16:34:43 -0600

    add test cases

--- 7_kyu/Reverse_Words/reverseWord.js
+++ 7_kyu/Reverse_Words/reverseWord.js
@@ -22,3 +22,14 @@ function reverseWords(str) {
   return solved
   console.log(solved)
 }
+
+/*
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(reverseWords('The quick brown fox jumps over the lazy dog.'), 'ehT kciuq nworb xof spmuj revo eht yzal .god');
+    assert.strictEqual(reverseWords('apple'), 'elppa');
+    assert.strictEqual(reverseWords('a b c d'), 'a b c d');
+    assert.strictEqual(reverseWords('double  spaced  words'), 'elbuod  decaps  sdrow');
+  });
+});
+*/

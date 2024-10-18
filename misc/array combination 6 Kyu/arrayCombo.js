0b4b1596170460c46f13c02e7cb7814eda7a1cfc
Kevin Ta <kvnta@yahoo.com>
2024-10-18 14:42:51 -0600

    add short solution

--- misc/array combination 6 Kyu/arrayCombo.js
+++ misc/array combination 6 Kyu/arrayCombo.js
@@ -29,4 +29,11 @@ Test.assertEquals(solve([[1,2],[4,4],[5,6,6]]),4);
 Test.assertEquals(solve([[1,2],[3,4],[5,6]]),8);
 Test.assertEquals(solve([[1,2,3],[3,4,6,6,7],[8,9,10,12,5,6]]),72);
 });
+
+
+
+OTHER SHORT SOLUTION:
+function solve(arr) {
+  return arr.reduce((res, curr) => res *= new Set(curr).size, 1);
+};
 */

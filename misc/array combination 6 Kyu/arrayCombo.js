73f884b90578ee6d1121a88fe60f6307f580cd1b
Kevin Ta <kvnta@yahoo.com>
2024-10-18 14:40:29 -0600

    add example test cases

--- misc/array combination 6 Kyu/arrayCombo.js
+++ misc/array combination 6 Kyu/arrayCombo.js
@@ -23,3 +23,10 @@ return answer
 console.log(answer);
   
 };
+/* describe("Basic tests", function(){
+Test.assertEquals(solve([[1,2],[4],[5,6]]),4);
+Test.assertEquals(solve([[1,2],[4,4],[5,6,6]]),4);
+Test.assertEquals(solve([[1,2],[3,4],[5,6]]),8);
+Test.assertEquals(solve([[1,2,3],[3,4,6,6,7],[8,9,10,12,5,6]]),72);
+});
+*/

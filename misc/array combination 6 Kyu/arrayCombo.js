53622e48111d0eb058fda07ecfd7a369224e4878
Kevin Ta <kvnta@yahoo.com>
2024-10-18 14:39:51 -0600

    add comments on explaining the solution

--- misc/array combination 6 Kyu/arrayCombo.js
+++ misc/array combination 6 Kyu/arrayCombo.js
@@ -11,8 +11,12 @@ Make sure that you don't count duplicates; for example solve([[1,2],[4,4],[5,6,6
 //all combos must be unique and no duplicates
 //
 function solve(arr) {
+  //assign variable. map itterates over array and sub arrays. 
+  //takes each subarry and represents as z. z returns a new array that hasbeen Set(removing duplicate numbers from sub arrays)
 let nodupe1 = arr.map(z => Array.from(new Set(z)))
-
+//variable answer takes the newly formed array without duplicates and calls reduce method on it.
+//the reduce has accumulator set to intiall 1. we take the length of each subarray and multiply
+//it by each accumulator to get the total number of unique outcomes possible for a given nested array set.
 let answer = nodupe1.reduce((a,b)=> a * b.length,1)
 
 return answer

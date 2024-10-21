ce72ffa78f8bdc798e32fe5bc523d73917a3818e
Kevin Ta <kvnta@yahoo.com>
2024-10-20 19:50:13 -0600

    add simple tests

--- 8_kyu/Array/duckDuckGoose.js
+++ 8_kyu/Array/duckDuckGoose.js
@@ -21,4 +21,29 @@ return players[position].name
 }
 
 
+//simple tests
+/*
+class Player {
+  constructor(name) {
+  	this.name = name;
+  }
+}
 
+let ex_names = ["a", "b", "c", "d", "c", "e", "f", "g", "h", "z"];
+let players = ex_names.map((n) => new Player(n));
+
+describe("fixed tests", function(){
+  it("should find the correct goose", function(){
+    assert.strictEqual(duckDuckGoose(players, 1),  "a");
+    assert.strictEqual(duckDuckGoose(players, 3),  "c");
+    assert.strictEqual(duckDuckGoose(players, 10), "z");
+    assert.strictEqual(duckDuckGoose(players, 20), "z");
+    assert.strictEqual(duckDuckGoose(players, 30), "z");
+    assert.strictEqual(duckDuckGoose(players, 18), "g");
+    assert.strictEqual(duckDuckGoose(players, 28), "g");
+    assert.strictEqual(duckDuckGoose(players, 12), "b");
+    assert.strictEqual(duckDuckGoose(players, 2),  "b");
+    assert.strictEqual(duckDuckGoose(players, 7),  "f");
+  });
+});
+*/

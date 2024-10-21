338bf0c3b41ad7fca6f8115b9e2fbc1b88aa0f6d
Kevin Ta <kvnta@yahoo.com>
2024-10-21 15:47:46 -0600

    add challenge link from codewars

--- 6_kyu/arrayCombo.js
+++ 6_kyu/arrayCombo.js
@@ -1,3 +1,5 @@
+//From https://www.codewars.com/kata/59e66e48fc3c499ec5000103
+
 /*In this Kata, you will be given an array of arrays and your task will be to return the number of unique arrays that can be formed by picking exactly one element from each subarray.
 
 For example: solve([[1,2],[4],[5,6]]) = 4, because it results in only 4 possibilites. They are [1,4,5],[1,4,6],[2,4,5],[2,4,6].
